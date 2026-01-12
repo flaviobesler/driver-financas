86c6ce82cb6f7eddea353fd6d83af59e3842640c
Flavio Besler <flavio.besler@gmail.com>
2026-01-12 08:04:49 -0400

    versão 1.3

--- js/criar_conta.js
+++ js/criar_conta.js
@@ -6,13 +6,61 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('signup-form');
   const msg = document.getElementById('msg');
 
+
+  function validarSenha(password){
+      const erros = [];
+
+      if(password.length < 8){
+        erros.push( 'minimo de 8 caracteres')};
+      
+      if(!/[a-z]/.test(password)){
+         erros.push( 'uma letra minuscula')};
+      
+      if(!/[A-Z]/.test(password)){
+         erros.push( 'uma letra maiúscula')};
+      
+      if(!/\d/.test(password)){
+         erros.push( 'um número')};
+      
+      if(!/[@$!%*#?&]/.test(password)){
+         erros.push( 'um simbulo')}; 
+      return erros;
+      }
+  
+  const inputSenha = document.getElementById('password')
+  inputSenha.addEventListener('input', () =>{
+
+    const erros = validarSenha(inputSenha.value);
+    if (erros.length > 0) {
+      msg.textContent = 'A senha precisa conter:\n' + erros.join(',\n ');
+      msg.style.color = 'red';
+      }else {
+      msg.textContent = '';
+      }
+    
+
+  })
+
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
-
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const password2 = document.getElementById('password2').value;
 
+      if(password!==password2){
+        msg.textContent ='as senhas não são iguais'
+        msg.style.color = 'red';
+      return;}
+
+      const erros = validarSenha(password);
+      if (erros.length > 0) {
+        msg.textContent = 'Corrija a senha antes de continuar';
+        msg.style.color = 'red';
+      return;
+      }
+      
     
+
 
     const { data, error } = await supabase.auth.signUp({
       email,
@@ -20,12 +68,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     if (error) {
-      
       msg.textContent = error.message;
       return;
     }
 
     msg.textContent = 'Conta criada. Verifique seu e-mail.';
+    msg.style.color = 'green';
 
   
 
--- js/redefinirsenha.js
+++ js/redefinirsenha.js
@@ -2,38 +2,72 @@ import { supabase } from "./supabaseClients.js";
 
 console.log('js carregou')
 
-document.getElementById('login').style.display = 'none';
-
-
-document.getElementById('redefinir').addEventListener('click', async () => {
-    
-    const senha1 = document.getElementById('senha1').value;
-    const senha2 = document.getElementById('senha2').value;
+document.addEventListener('DOMContentLoaded', () =>{
     const span = document.getElementById('span');
 
+    function validarSenha(senha1){
+      const erros = [];
 
-    if (senha1 !== senha2){
-    span.textContent= 'as senhas não estão iguais';
-    return;}
+      if(senha1.length < 8){
+        erros.push( 'minimo de 8 caracteres')};
+      
+      if(!/[a-z]/.test(senha1)){
+         erros.push( 'uma letra minuscula')};
+      
+      if(!/[A-Z]/.test(senha1)){
+         erros.push( 'uma letra maiúscula')};
+      
+      if(!/\d/.test(senha1)){
+         erros.push( 'um número')};
+      
+      if(!/[@$!%*#?&]/.test(senha1)){
+         erros.push( 'um simbulo')}; 
+      return erros;
+      }
 
-    if(senha1.length < 6){
-        span.textContent = 'a senha deve ter no minimo 6 caracteres'
-        return;
-    }
+    const inputSenha = document.getElementById('senha1')
+    inputSenha.addEventListener('input', () =>{
 
-    const {error} = await supabase.auth.updateUser({
-        password: senha1
+    const erros = validarSenha(inputSenha.value);
+    if (erros.length > 0) {
+      span.textContent = 'A senha precisa conter:\n' + erros.join(',\n ');
+      span.style.color = 'red';
+      }else {
+      span.textContent = '';
+      }
+  })
+
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('redefinir').addEventListener('click', async () => {
+        
+        const senha1 = document.getElementById('senha1').value;
+        const senha2 = document.getElementById('senha2').value;
+        
+      const erros = validarSenha(senha1);
+        if (erros.length > 0) {
+            span.textContent = 'Corrija a senha antes de continuar';
+            span.style.color = 'red';
+        return;}
+
+        if (senha1 !== senha2){
+        span.textContent= 'as senhas não estão iguais';
+        return;}
+
+
+        const {error} = await supabase.auth.updateUser({
+            password: senha1
+        })
+
+
+        if(error){
+            alert(error.message);
+        }else{
+            span.textContent = 'senha alterada com sucesso, retorne ao login'
+        }
+        document.getElementById('redefinir').style.display = 'none';
+        document.getElementById('login').style.display = 'block';
+        
+
     })
 
-
-    if(error){
-        alert(error.message);
-    }else{
-        span.textContent = 'senha alterada com sucesso, retorne ao login'
-    }
-    document.getElementById('redefinir').style.display = 'none';
-    document.getElementById('login').style.display = 'block';
-    
-
 })
-
