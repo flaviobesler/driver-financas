5f4a4bc78fd051a1032357bbf4fb773dd25f77c5
Flavio Besler <flavio.besler@gmail.com>
2026-02-01 19:21:26 -0400

    correção 6

--- js/assinatura.js
+++ js/assinatura.js
@@ -1,4 +1,5 @@
 import { supabase } from "./supabaseClients.js";
+
 document.addEventListener('DOMContentLoaded', async () => {
   const { data: { session } } = await supabase.auth.getSession();
   if (!session) {
