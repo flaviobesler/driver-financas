3ba8c82d15d213ddb3c9da717aa4dd3b575be203
Flavio Besler <flavio.besler@gmail.com>
2026-02-01 20:27:19 -0400

    correção 10

--- js/assinatura.js
+++ js/assinatura.js
@@ -1,14 +1,9 @@
 import { supabase } from "./supabaseClients.js";
+
 document.addEventListener('DOMContentLoaded', async () => {
   const { data: { session } } = await supabase.auth.getSession();
-
-  // Evita redirecionamento infinito se já estivermos na página de login
-  if (!session && window.location.pathname !== '/login.html') {
-    window.location.replace('/login.html');
+  if (!session) {
+    window.location.href = '/login.html';
     return;
-  }
-
-  if (session) {
-    console.log('Usuário logado:', session.user.email);
   }
 });
