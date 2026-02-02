6820550a331e27f3408f5314933a627f32e49b8d
Flavio Besler <flavio.besler@gmail.com>
2026-02-01 20:32:17 -0400

    agora vai?

--- js/assinatura.js
+++ js/assinatura.js
@@ -1,9 +1,14 @@
 import { supabase } from "./supabaseClients.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
+  const assinar = document.getElementById('assinar');
+  assinar.disabled =true
   const { data: { session } } = await supabase.auth.getSession();
   if (!session) {
+    assinar.disabled = true;
     window.location.href = '/login.html';
     return;
+  }else{
+    assinar.disabled =false
   }
 });
