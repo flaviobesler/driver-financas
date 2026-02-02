9f498026ab34d40da05bda727ba05879f87e6c51
Flavio Besler <flavio.besler@gmail.com>
2026-02-01 20:20:15 -0400

    correção 5

--- js/assinatura.js
+++ js/assinatura.js
@@ -3,8 +3,10 @@ import { supabase } from "./supabaseClients.js";
 document.addEventListener('DOMContentLoaded', async () => {
   const { data: { session } } = await supabase.auth.getSession();
   if (!session) {
-    window.location.href = '/login.html';
-    return;
-  }
+      setTimeout(() => {
+        window.location.replace('/login.html');
+      }, 50); // 50ms é suficiente
+      return;
+    }
 });
 
