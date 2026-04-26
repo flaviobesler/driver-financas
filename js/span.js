de9c51dc7bdb7ff5dabefe98b0f5b84a9e3f2e3f
Flavio Besler <flavio.besler@gmail.com>
2026-04-26 13:26:12 -0400

    atualização para versão 1.0

--- js/span.js
+++ js/span.js
@@ -1,2 +1,2 @@
-document.getElementById('spanversão').textContent = ' Versão 2.6.6';
+document.getElementById('spanversão').textContent = ' Versão 1.0.0';
 document.getElementById('spanAno').textContent = '2026';
